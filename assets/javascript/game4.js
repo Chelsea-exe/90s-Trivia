840a6f3de522f8796e37cc51579653e09346aa77
Chelsea-exe <cjackson145@student.gsu.edu>
2019-12-01 22:09:53 -0500

    new script v1

---
+++ assets/javascript/game4.js
@@ -0,0 +1,9 @@
+/*
+setting up variables for the start up/intro screen of the quiz and container 
+of the quiz to appear once start button has been clicked and timer has started
+*/
+let startButton = document.getElementById('start-btn')
+let quizIntro = document.getElementById('Intro')
+let quizContainer = document.getElementById('Container')
+
+
