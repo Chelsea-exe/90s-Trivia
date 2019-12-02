9eb3e4d7cfc6e9d7bf2940c2acffdfc06f3ed838
Chelsea-exe <cjackson145@student.gsu.edu>
2019-12-02 18:09:53 -0500

    update v2

--- assets/javascript/game4.js
+++ assets/javascript/game4.js
@@ -1,9 +1,23 @@
 /*
 setting up variables for the start up/intro screen of the quiz and container 
 of the quiz to appear once start button has been clicked and timer has started
-*/
 let startButton = document.getElementById('start-btn')
 let quizIntro = document.getElementById('Intro')
 let quizContainer = document.getElementById('Container')
 
+function hideForm ()
+startButton.addEventListener('click', startGame)
+function startGame() {
+    console.log('Game has started')
+}
 
+*/
+$(document).ready(function() {
+    $('#Container').hide();
+    $('#start-btn').on('click', function() {
+        $('#Container').toggle();
+        $('#Intro').toggle();
+        console.log("90's quiz has started!");
+
+    });
+});
