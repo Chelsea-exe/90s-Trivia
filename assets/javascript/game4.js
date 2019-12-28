52a0108552e7ac07026c4242c155fa78f7b33aa3
Chelsea-exe <cjackson145@student.gsu.edu>
2019-12-28 00:43:16 -0500

    Jquery update and timer added

--- assets/javascript/game4.js
+++ assets/javascript/game4.js
@@ -1,5 +1,6 @@
+var time = 120;
+var intervalId;
 window.onload = function() {
-    event.preventDefault();
     $('#Container').hide();
     $('#start-btn').on('click', function() {
         $('#Container').toggle();
@@ -7,10 +8,38 @@ window.onload = function() {
         $('#Score').hide();
         console.log("Quiz has started!");
     });
+    $('#start-btn').on('click', function() {
+        run();
+    })
+    function run() {
+        console.log("Timer has started");
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+    function decrement() {
+        time--;
+        $('#Clock').html("<h1>" + time + " " + "SECONDS LEFT" + "</h1>");
+        if (time === 0) {
+            alert("TIME IS UP, QUIZ IS OVER!!!");
+            stop();
+            console.log("Timer has stopped!");
+        }
+    }
+    function stop() {
+        clearInterval(intervalId);
+    }
     $('#submit-btn').on('click', function() {
         Validate();
     })
     function Validate () {
+        var skit = "In Living Color";
+        var girlPower = "Spice Girls";
+        var boyBand = "AJ, Brian, Nick, Kevin & Howie";
+        var tvShow = "Martin";
+        var nba = "Chicago Bulls";
+        var nfl = "New Orleans, Superdome";
+        var film = "Titanic";
+
         var skit = $('input[name = "skit"]:checked').val();
         console.log("skit!!", skit);
         var girlPower = $('input[name = "GP"]:checked').val();
@@ -36,47 +65,44 @@ window.onload = function() {
             alert("Answer #1 is correct");
         }
         else {
-            (skit === "The Brady Bunch", skit === "MADTv", skit === "Saturday Night Live");
-            alert("Answer #1 is wrong");
+            alert("Answer #1 is wrong = In Living Color");
+
         }
         if (girlPower === "Spice Girls") {
             alert("Answer #2 is correct");
         }
-        else (girlPower === "TLC", girlPower === "Destiny's Child", girlPower === "Dixie Chicks"); {
-            alert("Answer #2 is correct");
+        else {
+            alert("Answer #2 is wrong = Spice Girls");
         }
-        if (boyBand === "AJ, Brian, Nick, Kevin, Howie") {
+        if (boyBand === "AJ, Brian, Nick, Kevin & Howie") {
             alert("Answer #3 is correct");
         }
-        else (boyBand === "Justin, JC, Lance, Joey, Chris", boyBand === "Nick, Jeff Drew, Justin", 
-        boyBand === "Mark, Travis, Tom"); {
+        else {
             alert("Answer #3 is wrong");
         }
         if (tvShow === "Martin") {
             alert("Answer #4 is correct");
         }
-        else (tvShow === "Moesha", tvShow === "Friends", tvShow === "Sex and The City"); {
-            alert("Answer #4 is wrong");
+        else {
+            alert("Answer #4 is wrong = Martin");
         }
         if (nba === "Chicago Bulls") {
             alert("Answer #5 is correct");
         }
-        else (nba === "Houston Rockets", nba === "Los Angeles Lakers", nba === "San Antonio Spurs"); {
-            alert("Answer #5 is wrong");
+        else {
+            alert("Answer #5 is wrong = Chicago Bulls");
         }
         if (nfl === "New Orleans, Superdome") {
             alert ("Answer #6 is correct");
         }
-        else (nfl === "San Diego, Qualcomm Stadium", nfl === "Miami, Joe Robbie Stadium", 
-        nfl === "Pasadena, Rose Bowl"); {
-            alert("Answer #6 is wrong");
+        else  {
+            alert("Answer #6 is wrong = New Orleans, Superdome");
         }
-        if (film === "The Titantic") {
+        if (film === "The Titanic") {
             alert("Answer #7 is correct");
         }
-        else (film === "The Lion King", film === "Jurrasic Park", film === "The Color Purple"); {
-            alert("Answer #7 is wrong");
-            return
+        else {
+            alert("Answer #7 is wrong = The Titanic");
         }
     }
 }
